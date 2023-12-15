93277fefe77a28314d1731d4560ed9f7e7e424f3
annik <annikahauserbrydon@gmail.com>
2023-12-15 15:18:37 -0500

    contributions to tracker.html and leolaunch.js

--- js/leoLaunch.js
+++ js/leoLaunch.js
@@ -1,41 +1,27 @@
 // the URL that returns your JSON
 
-//use Satellite Situation Center API: https://sscweb.gsfc.nasa.gov/WebServices/REST/json/
+//using this database: https://celestrak.org/NORAD/elements/?FORMAT=json#:~:text=or%20so)%20Brightest-,Active%20Satellites,-Analyst%20Satellites
 
-//goal is to have api show which objects are passing above at a given location
-
-const url = "https://api.weather.gov/gridpoints/JAX/73,106/forecast/hourly"
-
-//31.260034, -81.360242, St. Simon's Island, GA
-
-// call the function to set everything in motion
-fetchRemoteData(url)
+const url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=last-30-days&FORMAT=json"
 
 
-//0 is to indicate which element is selected (eg the weather this afternoon)
+function buildPage(satelliteData) {
+	console.log(satelliteData);
+//I had to look to stack overflow for help since the JSON file had no properties/periods to follow.
+	if (satelliteData && satelliteData.length > 0) {
+	  const firstSatellite = satelliteData[0];
+	  const object_name = firstSatellite.OBJECT_NAME;
+	  const mean_motion = firstSatellite.MEAN_MOTION;
+  
+	  let container = document.querySelector(".satellite");
+	  let output = `The object ${object_name}'s mean motion is ${mean_motion}.`;
+  
+	  container.innerHTML = output;
+	} else {
+	  console.error("No satellite data found in the array.");
+	}
+  }
 
-function buildPage (weatherData) {
-	
-	console.log(weatherData)
-
-	results = weatherData
-
-	let container = document.querySelector(".forecast")
-
-	let weather = weatherData.properties.periods[0]
-
-	console.log(weather)
-	console.log(container)
-
-	console.log(weather[0])
-	let output = `The temp will be ${weather.temperature} and the forecast will be ${weather.shortForecast}.`
-	
-	container.innerHTML = output
-}
-
-// this function handles your business for you
-// and passes the results to the function above
-// you don't need to chane anything here
 async function fetchRemoteData(location) {
 	const response = await fetch(location)
 	if (!response.ok) {

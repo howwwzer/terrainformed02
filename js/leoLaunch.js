ecbb9bfc69ef6f497c85981754e58b23ec9212fb
annik <annikahauserbrydon@gmail.com>
2023-12-15 15:29:06 -0500

    nevermind

--- js/leoLaunch.js
+++ js/leoLaunch.js
@@ -6,21 +6,23 @@ const url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=last-30-days&FORM
 
 
 function buildPage(satelliteData) {
+	
 	console.log(satelliteData);
-//I had to look to stack overflow for help since the JSON file had no properties/periods to follow.
-	if (satelliteData && satelliteData.length > 0) {
-	  const firstSatellite = satelliteData[0];
-	  const object_name = firstSatellite.OBJECT_NAME;
-	  const mean_motion = firstSatellite.MEAN_MOTION;
-  
-	  let container = document.querySelector(".satellite");
-	  let output = `The object ${object_name}'s mean motion is ${mean_motion}.`;
-  
-	  container.innerHTML = output;
-	} else {
-	  console.error("No satellite data found in the array.");
-	}
-  }
+
+	results = satelliteData
+
+	let container = document.querySelector(".satellite")
+
+	let weather = satelliteDataData.properties.periods[0]
+
+	console.log(satellite)
+	console.log(container)
+
+	console.log(satellite[0])
+	let output = `The object ${object_name}'s mean motion is ${mean_motion}.`;
+	
+	container.innerHTML = output
+}
 
 async function fetchRemoteData(location) {
 	const response = await fetch(location)
